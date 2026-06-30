e9a4d623565790dd7ab438f73cd0535e6d2756ad
Ajay95087 <kajay777006@gmail.com>
2026-06-30 20:34:50 +0530

    30-06-2026

---
+++ ConsoleApp2/Switch/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 46;
+            if(num % 2 == 0)
+            {
+                Console.WriteLine("number is Even");
+            }
+            else
+            {
+                Console.WriteLine("number is Odd");
+            }
+        }
+    }
+}
