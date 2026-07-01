4acde683505e804d4065df2604a3764926e5c81a
Ajay95087 <kajay777006@gmail.com>
2026-07-01 10:07:15 +0530

    01-07-2026

---
+++ ConsoleApp2/Switch/Switch.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+namespace Switch
+{
+    internal class Switch
+    {
+        static void Main()
+        {
+            //int num = 46;
+            //if(num % 2 == 0)
+            //{
+            //    Console.WriteLine("number is Even");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("number is Odd");
+            //}
+
+            int day = 3;
+
+            switch (day)
+            {
+                case 1:
+                    {
+                        Console.WriteLine(" Monday");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine(" Tuesday");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine(" wednesday");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Numver is not match");
+                        break;
+                    }
+
+            }
+
+
+
+
+
+        }
+    }
+}
